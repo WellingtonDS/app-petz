6a5b6c3ce45bb882c8ce6ee718a61efbf67a0b32
wellington dos Santos <w_santos1993@outlook.com>
2023-03-30 19:23:30 -0300

    header e footer criados.

---
+++ src/components/header/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <nav>
+            <router-link to="/" class="logo">
+                <img src="@/assets/img/muralco-ar21.svg" alt="logoHeader">
+            </router-link>
+            <router-link class="button" v-if="$store.state.login" to="/user">
+                {{ name }}
+            </router-link>
+            <router-link v-else class="button" to="/login">login</router-link>
+        </nav>
+    </header>
+</template>
+
+<script>
+import HeaderComponent from './HeaderCoimponents';
+export default HeaderComponent;
+</script>
+
+<style lang="scss" scoped>
+@import'./Header.scss';
+</style>
